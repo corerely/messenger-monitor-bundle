8dc739bab4fa920d56ec8696a2e3519082de5b12
Andrii Oprysko <anjey.link@gmail.com>
2024-10-20 10:30:29 +0300

    fix namespace

--- src/MessengerMonitorBundle.php
+++ src/MessengerMonitorBundle.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace Corerely\MessengerMonitoreBundle;
+namespace Corerely\MessengerMonitorBundle;
 
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
