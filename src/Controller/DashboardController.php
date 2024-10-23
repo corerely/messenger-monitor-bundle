629fde7f93d61116b056421b11a8cad418499c31
Andrii Oprysko <anjey.link@gmail.com>
2024-10-23 20:32:24 +0300

    wip: test running controller

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,19 @@ namespace Corerely\MessengerMonitorBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use SymfonyCasts\MessengerMonitorBundle\FailedMessage\FailedMessageRepository;
+use SymfonyCasts\MessengerMonitorBundle\FailureReceiver\FailureReceiverProvider;
 
 abstract class DashboardController extends AbstractController
 {
     #[Route('/', name: 'corerely_messenger_monitor_dashboard', methods: ['GET'])]
-    public function index(): Response
+    public function index(
+        FailedMessageRepository $failedMessageRepository,
+    ): Response
     {
+
+        dd($failedMessageRepository);
+        dd('hit');
         return $this->render('@CorerelyMessengerMonitor/dashboard/index.html.twig');
     }
 }
